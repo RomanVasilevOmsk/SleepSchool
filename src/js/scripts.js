de90069d0a26a9d962136f69de49c3bf75e24b19
Roman Vasilev <zvezdarusy@gmail.com>
2020-01-30 11:26:47 +0600

    Styled film page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,55 @@
-// $( document ).ready(function() {
-//   console("setup");
-// });
+
+var accordion = function() {
+      var acc = document.getElementsByClassName("film__accordion-trigger");
+      var i;
+
+      for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+          this.classList.toggle("film__accordion-trigger--active");
+          var panel = this.nextElementSibling;
+          if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          }
+        });
+      }
+};
+
+var moreInfo = function() {
+  var acc = document.getElementsByClassName("film__info-trigger");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("film__info-trigger--active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+};
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  accordion.apply();
+  moreInfo.apply();
+});
+
+
+document.addEventListener("click", function(e) {
+  if (e.target.className=="film__accordion-trigger-play") {
+    e.stopPropagation();
+    var time = e.target.dataset.time;
+    var iframe = document.querySelector('iframe');
+    var player = new Vimeo.Player(iframe);
+
+    player.setCurrentTime(time).then(function() {
+      player.play();
+    })
+
+  }
+});
